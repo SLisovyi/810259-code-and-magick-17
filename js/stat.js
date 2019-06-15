ffa383795b48c5a1d8c2961fdbb554dc407c2314
Sergii Lisovyi <slisoviy@gmail.com>
2019-06-14 22:35:24 -0600

    Правка к Add Cloud

--- js/stat.js
+++ js/stat.js
@@ -6,6 +6,6 @@ var CLOUD_HEIGHT = 270;
 
 window.renderStatistic = function(ctx) {
   ctx.fillStyle = 'rgba(255, 255, 255, 1)';
-  ctx.fillRect = (x, y, CLOUD_WIDTh, CLOUD_HEIGHT);
+  ctx.fillRect = (cloud_x, cloud_y, CLOUD_WIDTh, CLOUD_HEIGHT);
 }
 
