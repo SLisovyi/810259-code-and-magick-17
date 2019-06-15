016cb067c60d6d05ce4b8a783da254fc3c35c4d7
Sergii Lisovyi <slisoviy@gmail.com>
2019-06-14 22:47:12 -0600

    Travis правки

--- js/stat.js
+++ js/stat.js
@@ -1,11 +1,13 @@
-var cloud_x = 100;
-var cloud_y = 10;
+'use strict'
+
+var CLOUD_X = 100;
+var CLOUD_Y = 10;
 
 var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
 
-window.renderStatistic = function(ctx) {
+window.renderStatistic = function (ctx) {
   ctx.fillStyle = 'rgba(255, 255, 255, 1)';
-  ctx.fillRect = (cloud_x, cloud_y, CLOUD_WIDTh, CLOUD_HEIGHT);
-}
+  ctx.fillRect = (CLOUD_X, CLOUD_Y, CLOUD_WIDTH, CLOUD_HEIGHT);
+};
 
