9f9a88674ef6c5c8ddddecebb8fcb1d6da676fb3
Sergii Lisovyi <slisoviy@gmail.com>
2019-06-21 20:57:49 -0600

    add times array and names

--- js/stat.js
+++ js/stat.js
@@ -1,53 +1,64 @@
 'use strict';
 
+var CLOUD_WIDTH = 500;
+var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
 var CLOUD_Y = 10;
+var GAP = 10;
+var BAR_GAP = 50;
+var FONT_GAP = 80;
+var TEXT_HEIGTH = 20;
+var BAR_WIDTH = 40;
+var barY = (CLOUD_HEIGHT + CLOUD_Y - GAP - TEXT_HEIGTH);
 
-var CLOUD_SHADOW_X = 110;
-var CLOUD_SHADOW_Y = 20;
+var renderCloud = function (ctx, x, y, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_WIDTH);
+};
 
-var CLOUD_WIDTH = 420;
-var CLOUD_HEIGHT = 270;
+// находим максимальный элемент массива
+var getMaxElement = function (arr) {
+  var maxElement = arr[0];
+  for (var i = 1; i < arr.length; i++) {
+    if (arr[i] > maxElement) {
+      maxElement = arr[i];
+    }
+  }
+  return maxElement;
+};
 
-var PLAYER_STAT_Y = 240;
+// находим рандомный цвет
+var getEnemiesColor = function (mass) {
+  var color = 'rgba(0, 0, 255, 0.' + Math.floor(3 + Math.random() * 9) + ')';
+  if (mass === 'Вы') {
+    color = 'rgba(255, 0, 0, 1)';
+  }
+  return color;
+};
 
-// Players Points
-var playerPoints1 = 150;
-var playerPoints2 = 120;
-var playerPoints3 = 50;
-var playerPoints4 = 70;
+window.renderStatistics = function (ctx, players, times) {
 
-window.renderStatistics = function (ctx) {
-  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-  ctx.fillRect(CLOUD_SHADOW_X, CLOUD_SHADOW_Y, CLOUD_WIDTH, CLOUD_HEIGHT);
+  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.3)');
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
 
-  ctx.fillStyle = 'rgba(255, 255, 255)';
-  ctx.fillRect(CLOUD_X, CLOUD_Y, CLOUD_WIDTH, CLOUD_HEIGHT);
+  var maxTime = getMaxElement(times);
+
+  for (var i = 0; i < players.length; i++) {
+
+    var barHeigth = (150 * times[i]) / maxTime;
+
+    ctx.fillStyle = '#000';
+    ctx.fillText(Math.floor(times[i]), CLOUD_X + GAP + FONT_GAP + (BAR_WIDTH + BAR_GAP) * i, barY - GAP - barHeigth);
+
+    ctx.fillText(players[i], CLOUD_X + GAP + FONT_GAP + (BAR_WIDTH + BAR_GAP) * i, CLOUD_Y + CLOUD_HEIGHT - GAP);
+
+    ctx.fillStyle = getEnemiesColor(players);
+    ctx.fillRect(CLOUD_X + GAP + FONT_GAP + (BAR_WIDTH + BAR_GAP) * i, barY - barHeigth, BAR_WIDTH, barHeigth);
+  }
 
   ctx.fillStyle = 'rgba(0, 0, 0)';
   ctx.font = '16px PT Mono';
   ctx.textBaseline = 'hanging';
-  ctx.fillText('Ура вы победили!', 180, 20);
-  ctx.fillText('Список результатов:', 180, 40);
-
-  // 1st Player Column
-  ctx.fillStyle = 'red';
-  ctx.fillRect(170, (PLAYER_STAT_Y - playerPoints1), 40, playerPoints1);
-  // ctx.fillStyle = "#000000";
-  // ctx.font = '16px PT Mono';
-  // ctx.fillText(wizardTimes[0], 180, 20);
-
-  // 2nd Player Column
-  ctx.fillStyle = 'blue';
-  ctx.fillRect(260, (PLAYER_STAT_Y - playerPoints2), 40, playerPoints2);
-  // 3rd Player Column
-  ctx.fillStyle = 'grey';
-  ctx.fillRect(350, (PLAYER_STAT_Y - playerPoints3), 40, playerPoints3);
-  // 4th Player Column
-  ctx.fillStyle = 'green';
-  ctx.fillRect(440, (PLAYER_STAT_Y - playerPoints4), 40, playerPoints4);
+  ctx.fillText('Ура вы победили!', 170, 20);
+  ctx.fillText('Список результатов:', 170, 40);
 };
-
-// window.renderStatistics = function (times) {
-//   var wizardTimes = [];
-// }
