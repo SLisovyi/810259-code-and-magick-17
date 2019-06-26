1cf1e26bde93e3ca3d3b2ca2efeba532b24c8b65
Sergii Lisovyi <slisoviy@gmail.com>
2019-06-25 21:48:30 -0600

    start module4-task1

--- js/setup.js
+++ js/setup.js
@@ -36,3 +36,56 @@ for (var i = 0; i < 4; i++) {
   var wizardEye = document.querySelector('#similar-wizard-template').content.querySelector('.wizard-eyes');
   wizardEye.style.fill = randomArrayItem(wizardEyeColors);
 }
+
+// ---------------- открытие окна настройки персонажа ------------------
+var setupWindow = document.querySelector('.setup');
+var setupOpenButton = document.querySelector('.setup-open');
+var setupCloseButton = setupWindow.querySelector('.setup-close');
+
+setupOpenButton.addEventListener('click', function () {
+  setupWindow.classList.remove('hidden');
+});
+
+setupCloseButton.addEventListener('click', function () {
+  setupWindow.classList.add('hidden');
+});
+
+document.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === 27) {
+    setupWindow.classList.add('hidden');
+  }
+});
+
+document.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === 13) {
+    setupWindow.classList.remove('hidden');
+  }
+});
+
+// ---------------- меняем цвета персонажа при клике ------------------
+var setupWizardCoat = setupWindow.querySelector('.wizard-coat');
+var setupWizardEye = setupWindow.querySelector('.wizard-eyes');
+var setupWizardFireball = setupWindow.querySelector('.setup-fireball-wrap');
+
+// находим формы для inputs заполнения
+var inputWizardCoatColor = document.getElementsByName('coat-color');
+var inputWizardEyeColor = document.getElementsByName('eyes-color');
+var inputWizardFireballColor = document.getElementsByName('fireball-color');
+
+// массивы цветов
+var setupWizardCoatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var setupWizardEyeColors = ['black', 'red', 'blue', 'yellow', 'green'];
+var setupWizardFireballColors = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
+
+var getSetupColors = function (element, colors, input) {
+  element.addEventListener('click', function () {
+    var random = randomArrayItem(colors);
+
+    element.style.fill = random;
+    input.value = random;
+  });
+};
+
+getSetupColors(setupWizardCoat, setupWizardCoatColors, inputWizardCoatColor);
+getSetupColors(setupWizardEye, setupWizardEyeColors, inputWizardEyeColor);
+getSetupColors(setupWizardFireball, setupWizardFireballColors, inputWizardFireballColor);
